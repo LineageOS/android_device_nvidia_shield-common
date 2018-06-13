a97b34ac21207d216bc629b364850279b3133c14
Aaron Kling <webgeek1234@gmail.com>
2018-06-12 21:33:46 -0500

    Limit scope of shims
    Change-Id: Ia5fc2c5980bd357b4e641c9b0d7ae73817c21e35

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ TARGET_THERMALHAL_VARIANT := tegra
 TARGET_RELEASETOOLS_EXTENSIONS := device/nvidia/shield-common/releasetools
 
 # Forced shims
-TARGET_LD_SHIM_LIBS := /system/vendor/bin/glgps_nvidiaTegra2android|libsensor.so:/system/lib/libcutils.so|libcutils_shim.so:/system/lib64/libcutils.so|libcutils_shim.so:/system/lib/libicuuc.so|libicu_shim.so
+TARGET_LD_SHIM_LIBS := /system/vendor/bin/glgps_nvidiaTegra2android|libsensor.so:/system/vendor/lib/libril-icera.so|libcutils_shim.so::/system/vendor/lib/hw/audio.primary.tegra.so|libicu_shim.so
 
 # Use unified vendor
 TARGET_TEGRA_VARIANT := shield
