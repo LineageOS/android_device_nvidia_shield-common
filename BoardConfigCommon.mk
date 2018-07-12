e2de808a969da434ca03789de14e05d645627453
Aaron Kling <webgeek1234@gmail.com>
2018-07-12 18:37:34 -0500

    Allow memtrack hal to access nvmap stats
    Change-Id: I5ecf99d9d13a4ab52150c90e6ece72570d501abb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,7 +15,8 @@
 #
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/nvidia/shield-common/sepolicy/common
+BOARD_SEPOLICY_DIRS += device/nvidia/shield-common/sepolicy/common \
+                       device/nvidia/shield-common/sepolicy/lineage-common
 
 ifneq ($(filter $(TARGET_TEGRA_MODEM),icera),)
 	BOARD_SEPOLICY_DIRS += device/nvidia/shield-common/sepolicy/icera
