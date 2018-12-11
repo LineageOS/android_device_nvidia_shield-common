f4f548928546ffc2f2b201df1c37b5607a0d2cdb
Aaron Kling <webgeek1234@gmail.com>
2018-12-11 10:56:09 -0600

    Add vendor hook for service restart consolemode
    This is used by Nvidias Console Mode app. The app sets a few props,
    changes the window managers display resolution, then issues a
    command to zygote to restart all services. This handles that
    command.
    
    Change-Id: I1e3f409b2efe5bdc9a542a44eb2ad3198e54a0ac

---
+++ init/service_shield.cpp
@@ -0,0 +1,26 @@
+#include <errno.h>
+#include "service.h"
+
+#include "service_shield.h"
+
+int shield_handle_control_message(const std::string &msg, const std::string &arg)
+{
+    android::init::Service *sf_svc = NULL;
+    android::init::Service *zg_svc = NULL;
+
+    if (!msg.compare("restart") && !arg.compare("consolemode")) {
+        sf_svc = android::init::ServiceList::GetInstance().FindService("surfaceflinger");
+        zg_svc = android::init::ServiceList::GetInstance().FindService("zygote");
+
+        if (sf_svc && zg_svc) {
+            zg_svc->Stop();
+            sf_svc->Stop();
+            sf_svc->Start();
+            zg_svc->Start();
+        }
+
+        return 0;
+    }
+
+    return -EINVAL;
+}
---
+++ init/service_shield.h
@@ -0,0 +1,6 @@
+#ifndef __SERVICE_SHIELD__H__
+#define __SERVICE_SHIELD__H__
+
+int shield_handle_control_message(const std::string&, const std::string&);
+
+#endif /* __SERVICE_SHIELD__H__ */
