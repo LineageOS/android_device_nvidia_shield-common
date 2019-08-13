3d7dd1ed619a52722c4daa403bd59920e0d69728
Aaron Kling <webgeek1234@gmail.com>
2019-08-13 13:26:15 -0500

    Enable nvaccessories
    Change-Id: I0115b7f21fa7c471f6aa165e582c7b48c9c05b64

--- shield.mk
+++ shield.mk
@@ -18,7 +18,8 @@ TARGET_TEGRA_VARIANT := shield
 
 -include vendor/lineage/product/nvidia.mk
 ifeq ($(NV_ANDROID_FRAMEWORK_ENHANCEMENTS),true)
-TARGET_TEGRA_TEGRAZONE := true
+TARGET_TEGRA_NVACCESSORIES := true
+TARGET_TEGRA_TEGRAZONE     := true
 endif
 
 # Overlay
