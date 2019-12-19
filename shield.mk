81a131a0ca27a94e71a01d2b908a2e5307c141ed
Nolen Johnson <johnsonnolen@gmail.com>
2019-12-19 02:38:18 +0000

    shield-common: Build hwc2on1 adapter on strictly t210+
    Change-Id: Iea525c7e5b7f9e73584be915ce4e7e46de604feb

--- shield.mk
+++ shield.mk
@@ -147,7 +147,6 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
 # Renderscript deps
