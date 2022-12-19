603b67230c518c4ea5a2fff3ba49de4865352e28
Nolen Johnson <johnsonnolen@gmail.com>
2022-12-19 14:01:59 -0500

    vendor: Drop TegraZone
    * Deprecated, and huge, maxes out sif's vendor size.
    
    Change-Id: I217c6b32850c4093a9627df4bca17c286c57f1e7

--- vendor/TegraZone/tegrazone.mk
+++ vendor/TegraZone/tegrazone.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_PACKAGES += \
-                    TegraZone_Next
+#PRODUCT_PACKAGES += \
+#                    TegraZone_Next
